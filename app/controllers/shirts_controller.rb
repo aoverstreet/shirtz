cb959a38fe6237d253a4fc1c17d2d69c52da4489
adam <adam.overstreet@gmail.com>
2013-07-18 20:55:45 -0400

    adding new files

--- app/controllers/shirts_controller.rb
+++ app/controllers/shirts_controller.rb
@@ -1,5 +1,7 @@
 class ShirtsController < ApplicationController
 
+	  before_action :load_shirt, only: [:edit, :update, :show, :destroy]
+
 	def index
 		#render :text => "Something Simple"
 		
@@ -11,8 +13,71 @@ class ShirtsController < ApplicationController
 		
 		#@shirts = Shirt.where("name LIKE :query", query: "%#{params[:query]}%")
 		
+		#much easier
 		@shirts = Shirt.search_for(params[:query])
 		
 	end
+	
+	def show
+	end
+	
+	def new
+		@shirt = Shirt.new
+	end
+	
+	def edit
+	end
+	
+	def update
+		#load_shirt
+		@shirt.update shirt_params
+		
+		#try and save
+		if @shirt.save
+			redirect_to @shirt
+		else
+			render :edit
+		end
+	end
+	
+	
+	
+	def create
+		@shirt = Shirt.new(shirt_params)
+		
+		#try and save
+		if @shirt.save
+			redirect_to @shirt
+		else
+			render :new 
+		end
+
+	end
+	
+	def destroy
+		
+		
+ 	
+	
+		if @shirt.destroy
+			flash[:notice] = "The '#{@shirt.name}' shirt has been removed. "
+			redirect_to shirts_path
+		else
+			redirect_to @shirt
+		end
+	
+	end
+	
+	private
+	
+	def load_shirt
+		@shirt = Shirt.find(params[:id])
+	end
+	
+	#require that they have a shirt and get these attributes
+	def shirt_params
+		params.require('shirt').permit(:name, :description, :image)
+	end
+	
 
 end
--- app/models/shirt.rb
+++ app/models/shirt.rb
@@ -1,4 +1,10 @@
 class Shirt < ActiveRecord::Base
+
+	#validation
+	validates :name, :description, :image,  presence: true
+	validates  :name, :description, length: {minimum: 10} 
+	#validates :image, format: 
+	
 	
 	def self.search_for(query)
 		Shirt.where("name LIKE :query OR description LIKE :query", query: "%#{query}%")
